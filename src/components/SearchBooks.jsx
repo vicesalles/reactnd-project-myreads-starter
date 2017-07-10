28a2526e3a49b99fd9464ff4d304428c1babf38b
Vice Sallés <vicenc.salles@gmail.com>
2017-07-10 07:31:49 +0200

    style: Clean and tidy

--- src/components/SearchBooks.jsx
+++ src/components/SearchBooks.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI'
 
 import SearchBar from './SearchBar.jsx';
+import Book from './Book.jsx';
 
 
 export default class BookExplorer extends Component {
+
+    state = {
+        books: []
+    }
+
+    newSearch = (book) => {
+
+        BooksAPI.search(book).then((res) => {
+           
+            let nBooks = [];
+            res.map((b) => {
+
+                return nBooks.push(<Book key={b.id} title={b.title} cover={b.imageLinks.thumbnail} author={b.author} />);
+
+            });
+
+            this.setState({ books: nBooks });
+
+        }
+
+        );
+
+    }
+
     render() {
         return (
             <div className="search-books">
-                <SearchBar/>
+                <SearchBar onChange={this.newSearch} />
                 <div className="search-books-results">
-                    <ol className="books-grid"></ol>
+                    <ol className="books-grid">
+                        {this.state.books.length > 0 && this.state.books}
+                    </ol>
                 </div>
             </div>
         );
