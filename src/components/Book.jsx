383a6dd8d341744f310b05b9040c20ba1d1c20b6
Vice Sallés <vicenc.salles@gmail.com>
2017-07-11 06:44:54 +0200

    feat: Can change book from shelf to shelf

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import * as BooksAPI from '../BooksAPI';
 
 import ShelfChanger from './ShelfChanger.jsx'
 
 export default class Book extends Component {
+
+    newShelf = (id, s) => {
+        BooksAPI.update({ id }, s);
+    }
+
     render() {
         return (
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.cover})` }}></div>
-                   <ShelfChanger/>
+                    <ShelfChanger onChange={(s) => this.newShelf(this.props.id, s)} />
                 </div>
                 <div className="book-title">{this.props.title}</div>
                 <div className="book-authors">{this.props.author}</div>
--- src/components/ShelfChanger.jsx
+++ src/components/ShelfChanger.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 export default class ShelfChanger extends Component {
+
+    handleChange = (shelf) => {
+        this.props.onChange(shelf);
+    }
+
     render() {
         return (
             <div className="book-shelf-changer">
-                <select>
+                <select onChange={(e) => this.handleChange(e.target.value)}>
                     <option value="none" disabled>Move to...</option>
                     <option value="currentlyReading">Currently Reading</option>
                     <option value="wantToRead">Want to Read</option>
