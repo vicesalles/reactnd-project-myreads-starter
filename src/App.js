bb21e71fd60cd4c4ea77c06fb60cc9bbe5dee2f1
Vice Sallés <vicenc.salles@gmail.com>
2017-07-11 06:43:40 +0200

    feat: Delete duplicated elements of a given Array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import * as BooksAPI from './BooksAPI'
+import React from 'react';
 import { Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
+import _ from 'lodash';
 
 import MyLibrary from './components/MyLibrary.jsx';
 import SearchBooks from './components/SearchBooks.jsx';
@@ -11,16 +11,24 @@ class BooksApp extends React.Component {
 
   }
 
+  /**
+   * @description Elimninates duplicated items from an array
+   * @param {array} a
+   */
+  noDuplicatedBooks = (a) => {
+    return _.uniqueId(a,'id');
+  }
+
   render() {
     return (
       <div className="app">
 
         <Route exact path="/" render={() => (
-          <MyLibrary />
+          <MyLibrary noDuplicate={this.noDuplicatedBooks} />
         )} />
 
         <Route path="/search" render={() => (
-          <SearchBooks />
+          <SearchBooks noDuplicate={this.noDuplicatedBooks} />
         )} />
 
       </div>
