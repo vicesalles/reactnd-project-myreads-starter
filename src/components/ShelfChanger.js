0a61486b8e2da2978a5eadcb829465afe7534dfb
Vice Sallés <vicenc.salles@gmail.com>
2017-07-17 16:34:47 +0200

    fix: Default Value of option now is correct

--- src/components/ShelfChanger.js
+++ src/components/ShelfChanger.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import myShelves from '../myShelves';
 
 export default class ShelfChanger extends Component {
 
@@ -15,9 +16,13 @@ export default class ShelfChanger extends Component {
 
         if (shelf !== 'none' && shelf !== '') {
 
-            let s = this.stringShelf(shelf);
-            return <option disabled defaultValue>&#10004; {s}</option>
+            return this.createOptions(shelf);
 
+        }else{
+
+            let options = this.createOptions(shelf);    
+
+            return [<option key="none" value="none" style={this.theHidden} defaultChecked defaultValue></option>,...options];
         }
 
     }
@@ -46,16 +51,34 @@ export default class ShelfChanger extends Component {
         return r;
     }
 
+
+    /**
+     * @description Creates the option list for the select node rendered by this component
+     * @param {String} s current Shelf, if any, for the book 
+     * @return {Array} An array with all <option> nodes needed
+     */
+    createOptions(s){
+        
+        let myOptions = myShelves.map((currentShelf)=>{
+            if(s===currentShelf){
+                return <option key={currentShelf} value={currentShelf} defaultValue defaultChecked>{this.stringShelf(currentShelf)}</option>;
+            }else{
+                return <option key={currentShelf} value={currentShelf}>{this.stringShelf(currentShelf)}</option>;
+            }
+        });
+    
+        return myOptions;
+    }
+
+    theHidden = {display:'none'};
+
     render() {
+        
         return (
             <div className="book-shelf-changer">
-                <select onChange={(e) => this.handleChange(e.target.value)}>
+                <select defaultValue={this.props.shelf} onChange={(e) => this.handleChange(e.target.value)}>
                     <option value="none" disabled>Move to...</option>
-                    {this.hasShelf(this.props.shelf)}
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
+                    {this.hasShelf(this.props.shelf)}                    
                 </select>
             </div>
         );
